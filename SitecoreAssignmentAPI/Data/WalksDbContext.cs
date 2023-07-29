a6a136ed568fb6e497fe444f255473b25bc15814
Shubham Namdeo <s.namdeo@accenture.com>
2023-07-29 14:25:47 +0530

    appsettings update the connection string

---
+++ SitecoreAssignmentAPI/Data/WalksDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SitecoreAssignmentAPI.Models.Domain;
+
+namespace SitecoreAssignmentAPI.Data
+{
+    public class WalksDbContext:DbContext
+    {
+
+        // Later on we want to send our own connection through programe.cs that why usign DbContectOptions
+        public WalksDbContext(DbContextOptions dbContextOptions): base(dbContextOptions) 
+        { 
+        
+        }
+
+        // A Dbset is a property of DbContext class represent a collection of entities in the database (Difficulty, Region and walk)
+        // We have to repesent the DbSet as a collection of the entity  
+        // Below properties represent collection inside our database
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+
+    }
+}
