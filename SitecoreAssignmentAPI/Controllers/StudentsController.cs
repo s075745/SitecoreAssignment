0d349270f8ccbbef94fc8f11d3754a9510724265
Shubham Namdeo <s.namdeo@accenture.com>
2023-07-29 13:06:30 +0530

    adding nuget pacakages

--- SitecoreAssignmentAPI/Controllers/StudentsController.cs
+++ SitecoreAssignmentAPI/Controllers/StudentsController.cs
@@ -4,10 +4,20 @@ using Microsoft.AspNetCore.Mvc;
 namespace SitecoreAssignmentAPI.Controllers
 {
     // https://localost:portnumber/api/students
-    // port number we can get from launch setting.json
+    // port number we can get from launchsetting.json
     [Route("api/[controller]")]
     [ApiController]
     public class StudentsController : ControllerBase
     {
+
+        //GET: https://localost:portnumber/api/students
+        [HttpGet]
+        public IActionResult GetAllStudents() 
+        {
+            string[] studentNames = new string[] { "Shubham", "Namdeo" };
+            return Ok(studentNames);   
+        
+        }
+
     }
 }
