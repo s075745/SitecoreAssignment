78c089fbd5eb61a1ccd1740cbd2ad81875e3f61f
Shubham Namdeo <s075745@gmail.com>
2023-07-29 21:53:36 +0530

    Done upto finding the region by id

--- SitecoreAssignmentAPI/Controllers/RegionsController.cs
+++ SitecoreAssignmentAPI/Controllers/RegionsController.cs
@@ -1,40 +1,54 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using SitecoreAssignmentAPI.Data;
 using SitecoreAssignmentAPI.Models.Domain;
 
 namespace SitecoreAssignmentAPI.Controllers
 {
+    // Some important code is present which will demostrate the use of dependencey injection.
     // https://localhost:2345/api/regions
     [Route("api/[controller]")]
     [ApiController]
     public class RegionsController : ControllerBase
     {
-        // Get all the regions
-        // Get : https://localhost:portnumber/api/regions
+        private readonly WalksDbContext dbContext;
+
+        public RegionsController(WalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        // Get all regions
+        // Get: https://localhost:portnumber/api/regions
+
         [HttpGet]
         public IActionResult GetAll()
         {
-            var regions = new List<Region>
-            {
-                new Region
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Auckland Region",
-                    Code = "AKL",
-                    RegionImageUrl = "https://images.pexels.com/photos/3293150/pexels-photo-3293150.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=400&h=250&fit=crop&crop=focalpoint"
+            var regions = dbContext.Regions.ToList();
 
-                },
-                new Region
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Wellington Region",
-                    Code = "WLG",
-                    RegionImageUrl = "https://images.pexels.com/photos/346886/pexels-photo-346886.jpeg?auto=compress&cs=tinysrgb&w=600"
-
-                }
-            };
             return Ok(regions);
         }
+
+        // GET Singlle or get region by Id
+        // Get: https://localhost:portnumber/api/regions/{id}
+        [HttpGet]
+        [Route("{id}:Guid")]
+        public IActionResult GetById([FromRoute]  Guid id)
+        {
+
+           // var region = dbContext.Regions.Find(id);
+
+            var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if(region == null)
+            {
+                return NotFound();
+            }
+            return Ok(region);
+
+        }
+
 
     }
 }
