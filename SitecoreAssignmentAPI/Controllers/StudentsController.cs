687297ff3bd3fa898c8f66c716ad5621233926c7
Shubham Namdeo <s.namdeo@accenture.com>
2023-07-29 11:49:19 +0530

    Add project files.

---
+++ SitecoreAssignmentAPI/Controllers/StudentsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SitecoreAssignmentAPI.Controllers
+{
+    // https://localost:portnumber/api/students
+    // port number we can get from launch setting.json
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+    }
+}
