b56e539d5cc5072344bbf4ba62485c53f17e17cf
Shubham Namdeo <s075745@gmail.com>
2023-07-29 19:13:10 +0530

    Harded coded the value instead of database

---
+++ SitecoreAssignmentAPI/Controllers/RegionsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SitecoreAssignmentAPI.Models.Domain;
+
+namespace SitecoreAssignmentAPI.Controllers
+{
+    // https://localhost:2345/api/regions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        // Get all the regions
+        // Get : https://localhost:portnumber/api/regions
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = new List<Region>
+            {
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Auckland Region",
+                    Code = "AKL",
+                    RegionImageUrl = "https://images.pexels.com/photos/3293150/pexels-photo-3293150.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=400&h=250&fit=crop&crop=focalpoint"
+
+                },
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Wellington Region",
+                    Code = "WLG",
+                    RegionImageUrl = "https://images.pexels.com/photos/346886/pexels-photo-346886.jpeg?auto=compress&cs=tinysrgb&w=600"
+
+                }
+            };
+            return Ok(regions);
+        }
+
+    }
+}
--- SitecoreAssignmentAPI/Program.cs
+++ SitecoreAssignmentAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SitecoreAssignmentAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Added dependency injection for the DbContext classes and also provided connection string for this this DBContext that is WalksConnectionStrign
+builder.Services.AddDbContext<WalksDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("WalksConnectionString")));
 
 var app = builder.Build();
 
