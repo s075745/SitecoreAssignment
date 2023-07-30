b0cddeda70832474a3ec65858d28d42635c84fd9
Shubham Namdeo <s075745@gmail.com>
2023-07-30 18:52:36 +0530

    Consume Get Endpoint from the api

---
+++ Walks.Web/Controllers/RegionsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Walks.Web.Models.DTO;
+
+namespace Walks.Web.Controllers
+{
+    public class RegionsController : Controller
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public RegionsController(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<RegionDto> response = new List<RegionDto>();
+            try
+            {
+                //Get all regions from web api
+                var client = httpClientFactory.CreateClient();
+
+                var httpResponseMessage = await client.GetAsync("https://localhost:7156/api/regions");
+
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                response.AddRange(await httpResponseMessage.Content.ReadFromJsonAsync<IEnumerable<RegionDto>>());
+
+                ViewBag.Response = response;
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+            }
+
+            return View(response);
+        }
+    }
+}
